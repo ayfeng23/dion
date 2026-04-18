56a326fa8ed5b42b54a60c3c929b31e2e1448b52
ayfeng23 <fengyaustin0808@gmail.com>
2026-04-17 18:39:48 -0700

    wdschedule flip

--- dion/dion2.py
+++ dion/dion2.py
@@ -333,7 +333,7 @@ def dion2_post_orthogonalize(
     Inputs and outputs should be lists of regular Tensor, not DTensor.
     This is a separate function for compatibility with torch.compile().
     """
-    torch._foreach_mul_(X, 1 - base_lr * weight_decay)
+    # torch._foreach_mul_(X, 1 - base_lr * weight_decay)
 
     # Convert U to match parameter dtype
     dtype = X[0].dtype
@@ -343,7 +343,7 @@ def dion2_post_orthogonalize(
     U_scaled = [neg_lr * u for u in U]
     for x, u_scaled, idx in zip(X, U_scaled, indices):
         x.index_add_(dim=select_dim, index=idx, source=u_scaled)
-
+    torch._foreach_mul_(X, 1 - base_lr * weight_decay)
 
 # A helper function to print selection choice for each matrix
 # It only prints once `verbose` is set True
